3ac9ac11dfba72011cbac8bf5da9d4586ee01f9d
Nagashayan <nagashayan1@gmail.com>
2016-11-06 16:00:59 -0500

    Data normlaized for training set 1

--- classification.py
+++ classification.py
@@ -2,9 +2,15 @@
 #importing dataProcess file
 import dataProcessor as data
 
+
 #printing dataset 1
 dataset1 = data.TrainData1_df
+
+#our dataset is not homogeneous so need to convert to numeric type
+dataset1 = dataset1.convert_objects(convert_numeric=True).head()
+
+print 'Normalizing data set'
+dataset1 =  (dataset1 - dataset1.mean()) / (dataset1.max() - dataset1.min())
 print dataset1
-#trying to normalize data by using xi-min /(max-min) formula
-print dataset1.max(axis=0)
-print dataset1.min(axis=0)
+
+
