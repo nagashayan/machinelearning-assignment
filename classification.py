aa58a786142d7c32e84c5b3cd095914a63fc3bf7
Nagashayan <nagashayan1@gmail.com>
2016-11-06 16:19:33 -0500

    Normalizing  test data
    removed head() so entire dataframe is normalised now

--- classification.py
+++ classification.py
@@ -5,12 +5,23 @@ import dataProcessor as data
 
 #printing dataset 1
 dataset1 = data.TrainData1_df
+#printing dataset 1
+testset1 = data.TestData1_df
+
 
 #our dataset is not homogeneous so need to convert to numeric type
-dataset1 = dataset1.convert_objects(convert_numeric=True).head()
+dataset1 = dataset1.convert_objects(convert_numeric=True)
 
 print 'Normalizing data set'
 dataset1 =  (dataset1 - dataset1.mean()) / (dataset1.max() - dataset1.min())
+
 print dataset1
+
+#our testset is not homogeneous so need to convert to numeric type
+testset1 = testset1.convert_objects(convert_numeric=True)
+print 'Normalizing test set'
+testset1 =  (testset1 - testset1.mean()) / (testset1.max() - testset1.min())
+
+print testset1
 
 
