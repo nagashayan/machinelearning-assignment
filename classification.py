9ad7ddb6922040e9819eb6dd0dc2523e2b3bc7d0
Nagashayan <nagashayan1@gmail.com>
2016-11-14 17:22:02 -0500

    plotted transformed unnormlized data
    not much useful I think

--- classification.py
+++ classification.py
@@ -1,14 +1,17 @@
-
+import pandas as pd
 #importing dataProcess file
 import dataProcessor as data
 import pcaTest as pca
 import knnTest as knn
 import matplotlib.pyplot as plt
 
-
+#plotting normalized data
 pca.transformed_trainData1_norm.plot()
 plt.show()
 
 print knn.knnmatrix
 
-
+#plotting unnormalized data
+print pd.DataFrame(pca.transformed_trainData1)
+pd.DataFrame(pca.transformed_trainData1).plot(kind = 'bar')
+plt.show()
